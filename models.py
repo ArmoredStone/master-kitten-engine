0eeec6e3c61e2d475f465812d1c152b2d2f808ca
Alex Aheyev <aheyevalex@gmail.com>
2025-01-15 01:08:36 +0200

    index and static image resource handler, post request handler not working yet

--- models.py
+++ models.py
@@ -4,8 +4,5 @@ class OrderModel(db.Model):
     __tablename__ = "orders"
     
     id = db.Column(db.Integer, primary_key=True)
-    user_email = db.Column(db.String, db.CheckConstraint("email LIKE '%@%'"))
+    email = db.Column(db.String, db.CheckConstraint("email LIKE '%@%'"))
     quantity = db.Column(db.Integer, db.CheckConstraint("quantity > 0 AND quantity <= 100"))
-    
-    def __repr__(self):
-        return super().__repr__()
--- resources.py
+++ resources.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, request, jsonify, render_template, abort, send_from_directory
 from schemas import OrderSchema
 
 from flask.views import MethodView
@@ -7,22 +7,40 @@ from sqlalchemy.exc import SQLAlchemyError
 from models import OrderModel
 from db import db
 
+order_schema = OrderSchema()
 blp = Blueprint("main", __name__)
 
 @blp.route("/")
 def index():
     return render_template("index.html")
 
-@blp.route("/send_order")
-class Order(MethodView):
-    @blp.arguments(OrderSchema)
-    @blp.response(201, OrderSchema)
-    def post(self, order_data):
-        order = OrderModel(**order_data)
-        try:
-            db.session.add(order)
-            db.session.commit()
-        except SQLAlchemyError as e:
-            db.session.rollback()
-            abort(500, message=str(e))
-        return order
+@blp.route("/static/pictures/master-kitten-engine.png")
+def logo():
+    return send_from_directory('static/pictures', 'master-kitten-engine.png')
+
+@blp.route("/send_order", methods=['POST'])
+def create_order():
+    data = request.get_json()
+    
+    errors = order_schema.validate(data)
+    if errors:
+        return jsonify(errors), 400
+    
+    validated_data = order_schema.load(data)
+    
+    email = validated_data.get('email')
+    quantity = validated_data.get('quantity')
+    
+    new_order = OrderModel(email=email, quantity=quantity)
+    
+    try:
+        db.session.add(new_order)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        abort(500, message=str(e))
+    
+    result = order_schema.dump(new_order)
+    print(result)
+    
+    return jsonify({'message': 'Order created successfully!', 'order': result}), 201
